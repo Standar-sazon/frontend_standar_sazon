501b2514f678d2344f169792e6fe321a598b0a24
Shari <silver_899@hotmail.com>
2021-03-07 20:11:12 -0600

    First commit login

--- pages/login.js
+++ pages/login.js
@@ -13,6 +13,19 @@ export default function login () {
               </div>
               <div class='col-12 col-md-8'>
                 {/* content */}
+                <div className='access-card'>
+                  <h2>Inicia
+                    sesión
+                  </h2>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo distinctio nihil doloribus sed esse perferendis, officiis cum sequi amet repellendus facilis quisquam optio minus vitae.</p>
+                  <div className='d-flex justify-content-between align-items-center'>
+                    <p>Correo</p>
+                    <input class='enter-email' type='text' placeholder='example@mail.com' name='' id='' />
+                    <p>Contraseña</p>
+                    <input class='enter-email' type='text' placeholder='example@mail.com' name='' id='' />
+                    <button class='boton-gratis'>Inicia sesión</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
