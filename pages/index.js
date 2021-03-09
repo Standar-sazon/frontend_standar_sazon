3878a01a60bacbdfdbb0db56c3c2c48f0630ef78
jchz2 <jchz2@linux.huawei.net>
2021-03-09 02:04:02 -0600

    add hero

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import Layout from '../components/LayoutMaster'
+import HERO from '../public/img/heroimage.jpg'
+import LOGO_MOBILE from '../public/img/logo_hero_m.png'
+import ARROW from '../public/img/arrow.svg'
+import LOGO_DESKTOP from '../public/img/logo_hero_desk.png'
 
 export default function Home () {
   return (
     <Layout>
 
       <header className='container-fluid hero'>
-        <div className='row'>
+        <div className='row opacity-hero'>
           <div className='container'>
             <div className='row'>
               <div className='col-12'>
-                {/* Hero. */}
+                <img src={LOGO_MOBILE} alt='logo' className='d-md-none logo-mobile' />
+                <img src={LOGO_DESKTOP} alt='logo' className='d-none d-md-block logo-desktop' />
+                <div className='wrapper-btn'>
+                  <button className='btn_hero'>Crea tu cuenta ya!!</button>
+                  <img src={ARROW} alt='arrow' className='arrow_button' />
+                </div>
               </div>
             </div>
           </div>
