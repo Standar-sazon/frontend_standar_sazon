3100f257b10f36441052dd55c740f3b2e7abfd5b
Shari <silver_899@hotmail.com>
2021-03-10 11:52:29 -0600

    Agregar instrucciones

--- pages/recipes/create/utencils.js
+++ pages/recipes/create/utencils.js
@@ -2,18 +2,28 @@ import LayoutUser from '../../../components/LayoutUser'
 import React from 'react'
 import NextButton from '../../../components/NextButton'
 import Table from 'react-bootstrap/Table'
+import { Card } from 'react-bootstrap'
 
 const Utencilios = () => {
   return (
     <LayoutUser>
       <p>Utensilios</p>
       <div>
-        <Table borderless className='tittleResume'>
-          <td>Paso 1</td>
-          <button>Delete</button>
-        </Table>
+        <button className='buttonorange'>Olla express</button>
       </div>
-
+      <div>
+        <button className='buttongreen'>Sarten</button>
+        <p>Escribe tus instrucciones</p>
+        <Card className='card-table'>
+          <div className='form-input'>
+            <input type='text' placeholder='Pasos a seguir' name='' id='' />
+          </div>
+          <Table borderless className='tittleResume'>
+            <td>Paso 1</td>
+            <button>Delete</button>
+          </Table>
+        </Card>
+      </div>
       <div className='row justify-content-center'>
         <div className='col-12 col-lg-4'>
           <NextButton message='Siguiente' />
