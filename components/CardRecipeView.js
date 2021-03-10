6f3ce9d0afa5b75c9b9cb88a804acf42f8b0af14
arce <arcegutierrez.2508@gmail.com>
2021-03-10 17:33:06 -0600

    creación de card recipe view

---
+++ components/CardRecipeView.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import image from '../public/imagesRecipeView/Card_image_1.svg'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faDollarSign, faIndustry } from '@fortawesome/free-solid-svg-icons'
+
+const CardRecipeView = ({ title, icon, category, time, entry, price }) => {
+  return (
+    <container>
+      <row className='row row-cols-1 row-cols-md-3 g-4'>
+        <div className='col'>
+          <div className='card  cardRecipeView'>
+            <div className='image'>
+              <img
+                src={image}
+                layout='fill'
+                className='card-img image'
+                alt='Card steps in image'
+              />
+            </div>
+            <div className='card-body descriptionCardRecipe'>
+              <h5 className='card-title title'>{title}</h5>
+              <ul className='list-unstyled text-center card-content-how'>
+                <li className='category'>{category}</li>
+                <li className='entry'>{entry}</li>
+                <li className='time'>
+                  <FontAwesomeIcon icon={faClock} />1/2hr
+                </li>
+                <li className='price'>
+                  <FontAwesomeIcon icon={faDollarSign} /> {price}
+                </li>
+              </ul>
+              <div className='containerButtonRecipeView'>
+                <span className='iconWrapperRecipe'>
+                  <FontAwesomeIcon icon={icon} />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </row>
+    </container>
+  )
+}
+
+CardRecipeView.defaultProps = {
+  icon: faIndustry,
+  title: 'Alitas clásicas',
+  category: 'Categorías',
+  entry: 'Entradas',
+  time: '1/2 hr',
+  price: '150.00'
+}
+export default CardRecipeView
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,10 +1,16 @@
 import LayoutUser from '../../components/LayoutUser'
 import React from 'react'
+import CardRecipeView from '../../components/CardRecipeView'
 
 const Recipes = () => {
   return (
     <LayoutUser>
       <h1>Lista de recipes</h1>
+      <CardRecipeView>
+        backgroundSrc= ''
+        title:'Alitas Clásicas'
+      </CardRecipeView>
+
     </LayoutUser>
   )
 }
