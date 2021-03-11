8d77f74d8fee4bba13002690763d5d0e21707cb8
Juan Carlos Hdz <jchz2@icloud.com>
2021-03-11 02:24:40 -0600

    add form-init

--- pages/subrecipes/index.js
+++ pages/subrecipes/index.js
@@ -1,42 +1,38 @@
 import LayoutUser from '../../components/LayoutUser'
 import React from 'react'
-import ARROW_BLACK from '../../public/img/arrowblack.svg'
-import ARROW_GRAY from '../../public/img/arrowgray.svg'
 
 const SubRecipes = () => {
   return (
     <LayoutUser>
-      <div className='create-form-init d-flex justify-content-center align-items-center flex-column '>
-        <h1 className='first-data'>Primeros datos</h1>
-        <div className='from-data input1 d-flex flex-column '>
-          <label for=''>Nombre de la Receta*</label>
-          <input className='input' type='text' placeholder='Escribe el nombre de la receta' />
+      <form className='create-form-init'>
+        <h1 className='first-data text-center'>Primeros datos</h1>
+        <div className='d-flex flex-column flex-lg-row justify-content-between w-100 mt-5'>
+          <div className='form-input'>
+            <label for=''>Nombre de la Receta*</label>
+            <input className='input' type='text' placeholder='Escribe el nombre de la receta' />
+          </div>
+          <div className='form-input'>
+            <label for=''>Clasificación*</label>
+            <select className='input'>
+              <option selected>Clasificación del platillo</option>
+              <option>Entrada</option>
+              <option>Sopa</option>
+              <option>Plato fuerte</option>
+              <option>Postre</option>
+              <option>Bebida</option>
+              <option>Salsa</option>
+            </select>
+          </div>
         </div>
-        <div className='from-data input2  d-flex flex-column'>
-          <label for=''>Clasificación</label>
-          <select className='input'>
-            <option selected>Clasificación del platillo</option>
-            <option>Entrada</option>
-            <option>Sopa</option>
-            <option>Plato fuerte</option>
-            <option>Postre</option>
-            <option>Bebida</option>
-            <option>Salsa</option>
-          </select>
-          <p className='classific-text text-center'>*Elige entre nuestras categorías, la que más se ajunte al platillo que deseas preparar.</p>
-          <img src={ARROW_GRAY} alt='logo' className='arrowgray' />
+        <div className='d-flex flex-column flex-lg-row justify-content-between mt-lg-5'>
+          <div className='form-input'>
+            <label for=''>Fecha*</label>
+            <input className='input' type='date' placeholder='Captura la fecha' name='' id='' />
+          </div>
+          <p className='classific-text text-center mt-3'>*Elige entre nuestras categorías, la que más se ajunte al platillo que deseas preparar.</p>
         </div>
-        <div className='from-data input3 d-flex flex-column'>
-          <label for=''>Fecha</label>
-          <input className='input' type='date' placeholder='Captura la fecha' name='' id='' />
-        </div>
-        <div className='from-data d-flex felex-column'>
-          <button className='next-button'>Siguiente</button>
-          <img src={ARROW_BLACK} alt='logo' className='arrowblack' />
-        </div>
-      </div>
+      </form>
     </LayoutUser>
   )
 }
-
 export default SubRecipes
