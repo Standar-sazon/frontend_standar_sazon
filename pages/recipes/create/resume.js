c58f8871972d1deb5dc81707f39cf87db41f73c0
jchz2 <jchz2@icloud.com>
2021-03-12 23:53:36 -0600

    add section summary

--- pages/recipes/create/resume.js
+++ pages/recipes/create/resume.js
@@ -33,90 +33,90 @@ const Resume = () => {
         <div className='technical-section'>
           <div className='technical d-flex'>
             <div>
-              <lable className='title-resume'>Resumen</lable>
-              <lable className='main-title-resume'>Apartada Técnico</lable>
+              <p className='title-resume'>Resumen</p>
+              <p className='main-title-resume'>Apartada Técnico</p>
             </div>
             <img src={Logo} alt='logo' className='logo' />
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Producción</lable>
-            <lable className='unit-measurement green'>0.800 kgs</lable>
+            <p className='technical-concepts'>Producción</p>
+            <p className='unit-measurement green'>0.800 kgs</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Rendimiento</lable>
-            <lable className='unit-measurement orange unit'>60 %</lable>
+            <p className='technical-concepts'>Rendimiento</p>
+            <p className='unit-measurement orange unit'>60 %</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Tamaño de la porción</lable>
-            <lable className='unit-measurement orange unit'>400 Kgrs</lable>
+            <p className='technical-concepts'>Tamaño de la porción</p>
+            <p className='unit-measurement orange unit'>400 Kgrs</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Número de porciones</lable>
-            <lable className='unit-measurement green'>2 Unidades</lable>
+            <p className='technical-concepts'>Número de porciones</p>
+            <p className='unit-measurement green'>2 Unidades</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Tiempo de preparación</lable>
-            <lable className='unit-measurement green'>20 Minutos</lable>
+            <p className='technical-concepts'>Tiempo de preparación</p>
+            <p className='unit-measurement green'>20 Minutos</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Tiempo de cocción</lable>
-            <lable className='unit-measurement green'>15 Minutos</lable>
+            <p className='technical-concepts'>Tiempo de cocción</p>
+            <p className='unit-measurement green'>15 Minutos</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Temperatura de servicio</lable>
-            <lable className='unit-measurement green'>63 ºC</lable>
+            <p className='technical-concepts'>Temperatura de servicio</p>
+            <p className='unit-measurement green'>63 ºC</p>
           </div>
         </div>
 
         <div className='admin-section justify-content-center'>
           <div className='technical d-flex '>
             <div>
-              <lable className='title-resume'>Resumen</lable>
-              <lable className='main-title-resume'>Administrativo</lable>
+              <p className='title-resume'>Resumen</p>
+              <p className='main-title-resume'>Administrativo</p>
             </div>
             <img src={Logo} alt='logo' className='logo' />
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>costo total de insumos</lable>
-            <lable className='unit-measurement orange'>$ 75.70</lable>
+            <p className='technical-concepts'>costo total de insumos</p>
+            <p className='unit-measurement orange'>$ 75.70</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Costo de la mano de obra</lable>
-            <lable className='unit-measurement orange'>$ 9.12</lable>
+            <p className='technical-concepts'>Costo de la mano de obra</p>
+            <p className='unit-measurement orange'>$ 9.12</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Costo y gastos indirectos</lable>
-            <lable className='unit-measurement orange'>$ 22.71</lable>
+            <p className='technical-concepts'>Costo y gastos indirectos</p>
+            <p className='unit-measurement orange'>$ 22.71</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Costos y gastos por porción</lable>
-            <lable className='unit-measurement orange'>$ 53.77</lable>
+            <p className='technical-concepts'>Costos y gastos por porción</p>
+            <p className='unit-measurement orange'>$ 53.77</p>
           </div>
           <div className='totals d-flex justify-content-between'>
             <div className='technical totals'>
-              <lable className='technical-concepts totals-resume'>Precio de venta</lable>
-              <lable className='technical-concepts iva totals-resume'>por porción (S/IVA)</lable>
+              <p className='technical-concepts totals-resume'>Precio de venta</p>
+              <p className='technical-concepts iva totals-resume'>por porción (S/IVA)</p>
             </div>
             <p className='totals unit-measurement iva green'>$53.77</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>% del costo y gastos por porción</lable>
-            <lable className='unit-measurement orange'>70%</lable>
+            <p className='technical-concepts'>% del costo y gastos por porción</p>
+            <p className='unit-measurement orange'>70%</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>% de utilidad por porción</lable>
-            <lable className='unit-measurement orange'>30%</lable>
+            <p className='technical-concepts'>% de utilidad por porción</p>
+            <p className='unit-measurement orange'>30%</p>
           </div>
           <div className='technical d-flex'>
-            <lable className='technical-concepts'>Utilidad por porción</lable>
-            <lable className='unit-measurement orange'>$ 23.04</lable>
+            <p className='technical-concepts'>Utilidad por porción</p>
+            <p className='unit-measurement orange'>$ 23.04</p>
           </div>
           <div className='totals d-flex justify-content-between'>
             <div className='technical'>
-              <lable className='technical-concepts totals-resume'>Precio de venta</lable>
-              <lable className='technical-concepts iva totals-resume'>por porción (C/IVA)</lable>
+              <p className='technical-concepts totals-resume'>Precio de venta</p>
+              <p className='technical-concepts iva totals-resume'>por porción (C/IVA)</p>
             </div>
-            <lable className='unit-measurement green'>$89.10</lable>
+            <p className='unit-measurement green'>$89.10</p>
           </div>
         </div>
       </div>
