98999d016de66e60e4f2ac43d0237416ef68290d
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-03-17 19:23:30 -0600

    [UPDATE] Tray autocomplete

--- pages/recipes/create/ingredients.js
+++ pages/recipes/create/ingredients.js
@@ -1,4 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
 import LayoutUser from '../../../components/LayoutUser'
 
 import Table from 'react-bootstrap/Table'
@@ -7,8 +12,24 @@ import ShowIngredient from '../../../components/ShowIngredient'
 
 import { productsRequest } from '../../../services/product'
 
+const schema = yup.object().shape({
+  ingredient: yup.string().required('El campo es requerido'),
+  grossWeight: yup.number().min(1, 'Requerido').positive('Invalido').required('Requerido').typeError('Invalido'),
+  netWeight: yup.number().min(1, 'Requerido').positive('Invalido').required('Requerido').typeError('Invalido')
+})
+
 const Ingredients = () => {
   const [products, setProducts] = useState([])
+  const [ingredient, setIngredient] = useState('')
+  const [valueProductSelected, setValueProductSelected] = useState({})
+  const [ingredients, setIngredients] = useState([])
+
+  const { register, handleSubmit, getValues, setValue, errors } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+    reValidateMode: 'onChange'
+  })
+
   const getProducts = async () => {
     const response = await productsRequest()
     const responseJSON = await response.json()
@@ -19,46 +40,68 @@ const Ingredients = () => {
     console.log(productsAll)
     setProducts(productsAll)
   }, [])
-  const handleClickOption = (e) => {
-    console.log(e.target.value)
-  }
+
   const handleChangeInput = (e) => {
-    console.log('input')
     console.log(e.target.value)
+    const productFiltered = products.filter(product => product.name === e.target.value)
+    console.log(productFiltered)
+    const { grossWeight, netWeight } = getValues()
+    setValueProductSelected({
+      product: productFiltered[0]?._id,
+      priceUnit: productFiltered[0]?.priceUnit,
+      measureByBuy: productFiltered[0]?.measureByBuy,
+      image: productFiltered[0]?.image,
+      grossWeight,
+      netWeight
+    })
+    console.log(valueProductSelected)
+  }
+  const handleAddIngredient = ({ grossWeight, netWeight }) => {
+    setValueProductSelected({ ...valueProductSelected, grossWeight, netWeight })
+    console.log(valueProductSelected)
+    setIngredients([...ingredients, valueProductSelected])
+    // setValueProductSelected({})
+    // setIngredient('')
+    setValue('ingredient', '')
+    setValue('grossWeight', '')
+    setValue('netWeight', '')
   }
   return (
     <LayoutUser>
       <div className='section-add-ingredients'>
-        <form>
+        <form onSubmit={handleSubmit(handleAddIngredient)}>
           <div className='form-input'>
             <p>Agregar ingrediente</p>
             <div className='d-flex justify-content-between align-items-center'>
               <div className='d-flex flex-column'>
                 <label for=''>Ingrediente</label>
-                <input type='text' list='products' onChange={handleChangeInput} placeholder='Ingrediente' name='' id='' />
+                <input type='text' list='products' onChange={handleChangeInput} ref={register} placeholder='Ingrediente' name='ingredient' id='' />
+                <p>{errors.ingredient?.message}</p>
                 <datalist id='products'>
                   {
-                    products.map(product => (<option key={product._id} value={product._id}>{product.name}</option>))
+                    products.map(product => (<option key={product._id} data-id={product._id} value={product.name} />))
                   }
                 </datalist>
               </div>
               <div className='d-flex flex-column'>
-                <label for=''>Peso neto</label>
-                <input type='text' placeholder='Peso neto' name='' id='' />
+                <label for=''>Peso Bruto</label>
+                <input type='text' placeholder='Peso Bruto' ref={register} name='grossWeight' />
+                <p>{errors.grossWeight?.message}</p>
               </div>
               <div className='d-flex flex-column'>
-                <label for=''>Peso Bruto</label>
-                <input type='text' placeholder='Peso bruto' name='' id='' />
+                <label for=''>Peso Neto</label>
+                <input type='text' placeholder='Peso Neto' ref={register} name='netWeight' />
+                <p>{errors.netWeight?.message}</p>
               </div>
               <div className='d-flex flex-column'>
                 <p>Importe</p>
-                <p>$ 9</p>
+                <p>$ {valueProductSelected?.priceUnit}</p>
               </div>
               <div className='d-flex flex-column'>
                 <p>Unidad de M</p>
-                <p>- </p>
+                <p>- {valueProductSelected?.measureByBuy}</p>
               </div>
-              <button className='plusbutton'><span>+</span></button>
+              <button className='plusbutton' type='submit'><span>+</span></button>
             </div>
           </div>
         </form>
