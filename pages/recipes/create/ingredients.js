f00adc3b1e1b80f9d1db3781b5cdf72ae08402a4
jchz2 <jchz2@icloud.com>
2021-03-18 19:54:24 -0600

    add service

--- pages/recipes/create/ingredients.js
+++ pages/recipes/create/ingredients.js
@@ -7,7 +7,8 @@ import Table from 'react-bootstrap/Table'
 import NextButton from '../../../components/NextButton'
 import ShowIngredient from '../../../components/ShowIngredient'
 import { productRequest } from '../../../services/products'
-import faker from 'faker'
+import { subRecipeRequest } from '../../../services/subrecipes'
+/* import faker from 'faker' */
 
 const schema = yup.object().shape({
   product: yup.string().required('El campo es requerido'),
@@ -29,7 +30,18 @@ const Ingredients = () => {
     resolver: yupResolver(schema),
     mode: 'onBlur',
     reValidateMode: 'onChange'
+
   })
+
+  const [subRecipes, setSubRecipes] = useState([])
+  const [subRecipe, setSubRecipe] = useState({})
+  const [subRecipeSelected, setSubRecipeSelected] = useState([])
+  /* const { register, handleSubmit, errors, watch, setValue } = useForm({
+      resolver: yupResolver(schema),
+      mode: 'onBlur',
+      reValidateMode: 'onChange'
+
+    }) */
 
   const getAmount = (netWeight, priceUnit) => (netWeight * priceUnit).toFixed(2)
   const getPerformancePercent = (netWeight, grossWeight) => (netWeight / grossWeight) * 100
@@ -66,6 +78,41 @@ const Ingredients = () => {
     setValue('grossWeight', '')
   }
 
+  const errorClassIngrediente = errors.ingrediente ? 'error' : null
+  const errorClassSubRecipe = errors.ingrediente ? 'error' : null
+  const errorClassPesoBruto = errors.pesoBruto ? 'error' : null
+  const errorClassPesoNeto = errors.pesoNeto ? 'error' : null
+
+  useEffect(() => {
+    const performancePercent = getPerformancePercent(parseFloat(watchAllFields.netWeight), parseFloat(watchAllFields.grossWeight))
+    const amount = parseFloat(getAmount(parseFloat(watchAllFields.netWeight), subRecipes.priceUnit))
+    setSubRecipe({ ...subRecipes, netWeight: parseFloat(watchAllFields.netWeight), grossWeight: parseFloat(watchAllFields.grossWeight), performancePercent, amount })
+  }, [watchAllFields.netWeight, watchAllFields.grossWeight])
+
+  const getSubRecipe = async () => {
+    const response = await subRecipeRequest(localStorage.getItem('token'))
+    const responseJSON = await response.json()
+    return responseJSON.data
+  }
+
+  useEffect(async () => {
+    const subRecipes = await getSubRecipe()
+    setSubRecipes(subRecipes)
+  }, [])
+
+  const subRecipeHandleChange = (event) => {
+    const subRecipesName = event.target.value
+    const filteredSubRecipe = subRecipes.filter(subRecipe => subRecipe.name === subRecipesName)
+    setSubRecipe(filteredSubRecipe[0])
+  }
+
+  const onSubmitSubRecipes = ({ netWeight, grossWeight }) => {
+    setSubRecipeSelected([...subRecipeSelected, product])
+    setValue('product', '')
+    setValue('netWeight', '')
+    setValue('grossWeight', '')
+  }
+
   return (
     <LayoutUser>
       <div className='section-add-ingredients'>
@@ -75,24 +122,24 @@ const Ingredients = () => {
             <div className='d-flex justify-content-between align-items-center'>
               <div className='d-flex flex-column'>
                 <label>Ingrediente</label>
-                <input type='search' list='products' ref={register} onChange={handleChange} placeholder='Ingrediente' name='product' />
+                <input type='search' list='products' ref={register} onChange={handleChange} placeholder='Ingrediente' name='product' className={errorClassIngrediente} />
                 <datalist id='products'>
                   {
-                    ingredients.map(ingredient =>
-                      <option key={ingredient._id} value={ingredient.name} />
-                    )
-                  }
+                      ingredients.map(ingredient =>
+                        <option key={ingredient._id} value={ingredient.name} />
+                      )
+                    }
                 </datalist>
                 <p>{errors.product?.message}</p>
               </div>
               <div className='d-flex flex-column'>
                 <label>Peso Bruto</label>
-                <input type='text' ref={register} placeholder='Peso bruto' name='grossWeight' />
+                <input type='text' ref={register} placeholder='Peso bruto' name='grossWeight' className={errorClassPesoBruto} />
                 <p>{errors.grossWeight?.message}</p>
               </div>
-              <div className='d-flex flex-column'>
+              <div className='d-flex flex-column '>
                 <label>Peso neto</label>
-                <input type='text' ref={register} placeholder='Peso neto' name='netWeight' />
+                <input type='text' ref={register} placeholder='Peso neto' name='netWeight' className={errorClassPesoNeto} />
                 <p>{errors.netWeight?.message}</p>
               </div>
               <div className='d-flex flex-column'>
@@ -108,34 +155,41 @@ const Ingredients = () => {
           </div>
         </form>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmitSubRecipes)}>
           <div className='form-input'>
             <p>Agregar Subreceta</p>
             <div className='d-flex justify-content-between align-items-center'>
-              <div className='d-flex flex-column'>
+              <div className='d-flex flex-column '>
                 <label>Subreceta</label>
-                <input type='text' placeholder='Ingrediente' name='subreceta' ref={register} />
+                <input className='error' type='text' placeholder='Ingrediente' name='subRecipes' onChange={subRecipeHandleChange} ref={register} />
+                <datalist id='subRecipes'>
+                  {
+                      subRecipes.map(subRecipe =>
+                        <option key={subRecipe._id} value={subRecipe.name} />
+                      )
+                    }
+                </datalist>
                 <p>{errors.subreceta?.message}</p>
               </div>
               <div className='d-flex flex-column'>
                 <label>Peso neto</label>
-                <input type='text' placeholder='Peso neto' name='pesoNeto' ref={register} />
+                <input type='text' placeholder='Peso neto' name='pesoNeto' ref={register} className={errorClassPesoNeto} />
                 <p>{errors.pesoNeto?.message}</p>
               </div>
               <div className='d-flex flex-column'>
                 <label>Peso bruto</label>
-                <input type='text' placeholder='Peso bruto' name='pesoBruto' ref={register} />
+                <input type='text' placeholder='Peso bruto' name='pesoBruto' ref={register} className={errorClassPesoBruto} />
                 <p>{errors.pesoBruto?.message}</p>
               </div>
               <div className='d-flex flex-column'>
                 <p>Importe</p>
-                <p>$ 11</p>
+                <p>$ {subRecipe?.priceUnit}</p>
               </div>
               <div className='d-flex flex-column'>
                 <p>Unidad de M</p>
-                <p>- </p>
+                <p>{subRecipe?.measureByBuy} </p>
               </div>
-              <button className='plusbutton'><span>+</span></button>
+              <button type='submit' className='plusbutton'><span>+</span></button>
             </div>
           </div>
         </form>
